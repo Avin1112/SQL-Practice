2399560841470a9dfc35908eae00112c68457058
Avinash Pandey <152147539+Avin1112@users.noreply.github.com>
2024-01-24 17:28:16 +0530

    Add files via upload

--- Challenge 2/challenge 2 answers.sql
+++ Challenge 2/challenge 2 answers.sql
@@ -1,22 +1,15 @@
--- Webpage link for the challenge : - https://www.steeldata.org.uk/sql2.html
+use practice2 ; 
 
-
--- Q1) What are the names of the players whose salary is greater than 100,000?
-
--- Answer)
+-- What are the names of the players whose salary is greater than 100,000?
 
 select player_name , salary from players where salary > 100000; 
 
- -- Q2) What is the team name of the player with player_id = 3?
-
--- Answer)
-
+ -- What is the team name of the player with player_id = 3?
+ 
  select t.team_id , t.team_name, pl.player_id from teams t inner join players pl on t.team_id = pl.team_id 
  where pl.player_id = 3; 
 
--- Q3) What is the total number of players in each team ?
-
--- Answer)
+-- What is the total number of players in each team ?
 
 -- Method 1
 
@@ -29,9 +22,7 @@ select team_id , team_name , count(player_id) as "Number of Players" from
                   inner join players pl on tm.team_id = pl.team_id ) table1
 group by team_id , team_name ; 
 
--- Q4) What is the team name and captain name of the team with team_id = 2 ?
-
--- Answer)
+-- What is the team name and captain name of the team with team_id = 2 ?
 
 -- Assumed Captain ID is same as Player ID
 
@@ -51,15 +42,11 @@ with table1 as
         from table1 )
 select * from team_name_with_captain where team_id = 2 ; 
 
--- Q5) What are the player names and their roles in the team with team_id = 1 ?
-
--- Answer)
+-- What are the player names and their roles in the team with team_id = 1 ?
 
 select player_name , role from players where team_id = 1 ; 
 
--- Q6) What are the team names and the number of matches they have won?
-
--- Answer)
+-- What are the team names and the number of matches they have won?
 
 -- Assumed Winner ID is same as Team ID.
 
@@ -78,9 +65,7 @@ select winner_id as "Winner's Team ID", team_name , count(*) as "Matches Won" fr
 group by winner_id , team_name ; 
 
 
--- Q7) What is the average salary of players in the teams with country 'USA'?
-
--- Answer)
+-- What is the average salary of players in the teams with country 'USA'?
 
 with table1 as
      ( select pl.player_name , pl.salary , t.country from teams t right join players pl on t.team_id = pl.team_id )
@@ -92,9 +77,7 @@ select round( avg(salary) , 2) as "Average Salary of Players (USA)" from
      ( select pl.player_name , pl.salary , t.country from teams t right join players pl on t.team_id = pl.team_id )table1
 where country = 'usa' ;
 
--- Q8) Which team won the most matches ?
-
--- Answer)
+-- Which team won the most matches ?
 
 with table1 as 
       ( select winner_id , count(*) from matches group by winner_id order by count(*) desc limit 1 ) ,
@@ -103,9 +86,7 @@ with table1 as
         from table1 t1 inner join teams t on t.team_id = t1.winner_id )
 select * from table2 ; 
 
--- Q9) What are the team names and the number of players in each team whose salary is greater than 100,000 ?
-
--- Answer)
+-- What are the team names and the number of players in each team whose salary is greater than 100,000 ?
 
 -- Method 1
 
@@ -128,8 +109,60 @@ select team_name , count(player_id) as "Players Count" from
 						  from teams t right join players pl on t.team_id = pl.team_id ) table1
 where salary > 100000 group by team_name ; 
 
--- Q10) What is the date and the score of the match with match_id = 3 ?
-
--- Answer)
+-- What is the date and the score of the match with match_id = 3 ?
 
 select match_id , match_date , score_team1 , score_team2 from matches where match_id = 3 ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
